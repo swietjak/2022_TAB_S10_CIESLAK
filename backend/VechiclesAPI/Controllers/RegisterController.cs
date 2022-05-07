aae7698bacc567b15d923264b68ecbbad99c5a38
Marta <marta.osm00@gmail.com>
2022-05-07 13:40:31 +0200

    added registration option

--- backend/VechiclesAPI/Controllers/RegisterController.cs
+++ backend/VechiclesAPI/Controllers/RegisterController.cs
@@ -1,18 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Tokens;
 using VehiclesAPI.Models;
 using VehiclesAPI.Dtos;
-using System;
-using System.IdentityModel.Tokens.Jwt;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
-using System.Linq;
+
 
 namespace VehiclesAPI.Controllers
 {
@@ -21,50 +11,53 @@ namespace VehiclesAPI.Controllers
     public class RegisterController : ControllerBase
     {
         private readonly WorldContext context;
-        private readonly UserManager<Worker> userManager;
 
-        public RegisterController(WorldContext context, UserManager<Worker> userManager)
+        public RegisterController(WorldContext context)
         {
             this.context = context;
-            this.userManager = userManager;
         }
 
         [HttpPost]
-        public async Task<IActionResult> Register([FromBody] RegisterDto value)
+        public IActionResult Register([FromBody] RegisterDto value)
         {
-            if(ModelState.IsValid){
-                var existingUser = await this.userManager.FindByEmailAsync(value.Email);
-                if(existingUser==null){
-                    Worker newWorker = new Worker();
-                    newWorker.Email=value.Email;
-                    newWorker.FirstName=value.FirstName;
-                    newWorker.Surname=value.LastName;
-                    newWorker.Password=value.Password;
-                    newWorker.Pesel=value.Pesel;
-                    if(value.Hascarepermissions.HasValue){
-                        newWorker.Hascarepermissions=value.Hascarepermissions.HasValue;
-                    }else 
-                    {
-                        newWorker.Hascarepermissions=false;
-                    }
-                    if(value.Isadmin.HasValue){
-                        newWorker.Isadmin=value.Isadmin.HasValue;
-                    }else 
-                    {
-                        newWorker.Isadmin=false;
-                    }
-                    
-                    IdentityResult result = await userManager.CreateAsync(newWorker,value.Password);
+            var existingUser = this.context.Workers.FirstOrDefault(existingUser => existingUser.Email.Equals(value.Email));
+            if(existingUser==null)
+            {
+                Worker newWorker = new Worker();
+                newWorker.Id=this.context.Workers.Count();
+                newWorker.Email=value.Email;
+                newWorker.FirstName=value.FirstName;
+                newWorker.Surname=value.LastName;
+                newWorker.Password=value.Password;
+                newWorker.Pesel=value.Pesel;
 
-                    if (result.Succeeded)
-                    {
-                        await userManager.AddToRoleAsync(newWorker, "Worker");
-                        return Created("", value);
-                    }
-                    
-                }  
+                if(value.Hascarepermissions.HasValue){
+                    newWorker.Hascarepermissions=value.Hascarepermissions.HasValue;
+                }else 
+                {
+                    newWorker.Hascarepermissions=false;
+                }
+                if(value.Isadmin.HasValue){
+                   newWorker.Isadmin=value.Isadmin.HasValue;
+                }else 
+                {
+                   newWorker.Isadmin=false;
+                }
+                this.context.Workers.Add(newWorker);
+                try
+                {
+                    this.context.SaveChanges();
+                    return StatusCode(201,value);
+                }
+                catch
+                {
+                    return StatusCode(400,"Failed registration");
+                }
             }
-            return StatusCode(400,"bibipoopoo");
+            else
+            {
+                return StatusCode(400,"User with this e-mail already exists");
+            }
         }
     }
 }
--- backend/VechiclesAPI/Dtos/Workers/RegisterDto.cs
+++ backend/VechiclesAPI/Dtos/Workers/RegisterDto.cs
@@ -6,9 +6,7 @@ namespace VehiclesAPI.Dtos
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        [Required(ErrorMessage = "Email is required")]
         public string Email { get; set; }
-        [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
         public string Pesel { get; set; }
         public bool? Hascarepermissions { get; set; }
